79501db7be82621e37010fa00374567c2fb008bf
jcm <jmcelwain@gmail.com>
2021-11-20 08:32:35 -0600

    refactor to use dyn trait.

--- cpp/RustCHOP.cpp
+++ cpp/RustCHOP.cpp
@@ -32,69 +32,49 @@ extern "C"
 
 DLLEXPORT
 void
-FillCHOPPluginInfo(CHOP_PluginInfo *info)
-{
+FillCHOPPluginInfo(CHOP_PluginInfo *info) {
     OperatorInfo chopInfo = chop_get_operator_info();
-	info->apiVersion = CHOPCPlusPlusAPIVersion;
-	info->customOPInfo.opType->setString(chopInfo.operator_type.c_str());
-	info->customOPInfo.opLabel->setString(chopInfo.operator_label.c_str());
-	info->customOPInfo.authorName->setString(chopInfo.author_name.c_str());
-	info->customOPInfo.authorEmail->setString(chopInfo.author_email.c_str());
-	info->customOPInfo.minInputs = chopInfo.min_inputs;
-	info->customOPInfo.maxInputs = chopInfo.max_inputs;
+    info->apiVersion = CHOPCPlusPlusAPIVersion;
+    info->customOPInfo.opType->setString(chopInfo.operator_type.c_str());
+    info->customOPInfo.opLabel->setString(chopInfo.operator_label.c_str());
+    info->customOPInfo.authorName->setString(chopInfo.author_name.c_str());
+    info->customOPInfo.authorEmail->setString(chopInfo.author_email.c_str());
+    info->customOPInfo.minInputs = chopInfo.min_inputs;
+    info->customOPInfo.maxInputs = chopInfo.max_inputs;
 }
 
 DLLEXPORT
-CHOP_CPlusPlusBase*
-CreateCHOPInstance(const OP_NodeInfo* info)
-{
-	// Return a new instance of your class every time this is called.
-	// It will be called once per CHOP that is using the .dll
-	return new RustCHOP(info);
+CHOP_CPlusPlusBase *
+CreateCHOPInstance(const OP_NodeInfo *info) {
+    return new RustCHOP(info);
 }
 
 DLLEXPORT
 void
-DestroyCHOPInstance(CHOP_CPlusPlusBase* instance)
-{
-	// Delete the instance here, this will be called when
-	// Touch is shutting down, when the CHOP using that instance is deleted, or
-	// if the CHOP loads a different DLL
-	delete (RustCHOP*)instance;
+DestroyCHOPInstance(CHOP_CPlusPlusBase *instance) {
+    delete (RustCHOP *) instance;
 }
 
 };
 
 
-RustCHOP::RustCHOP(const OP_NodeInfo* info)
-{
-    auto chop = chop_new();
-    this->chop = &chop;
+RustCHOP::RustCHOP(const OP_NodeInfo *info) {
+    chop = new BoxDynChop(chop_new());
 }
 
-RustCHOP::~RustCHOP()
-{
+RustCHOP::~RustCHOP() {
     dyn_chop_drop_in_place(chop);
 }
 
 void
-RustCHOP::getGeneralInfo(CHOP_GeneralInfo* ginfo, const OP_Inputs* inputs, void* reserved1)
-{
-	// This will cause the node to cook every frame
-	ginfo->cookEveryFrameIfAsked = true;
-
-	// Note: To disable timeslicing you'll need to turn this off, as well as ensure that
-	// getOutputInfo() returns true, and likely also set the info->numSamples to how many
-	// samples you want to generate for this CHOP. Otherwise it'll take on length of the
-	// input CHOP, which may be timesliced.
-	ginfo->timeslice = true;
-
-	ginfo->inputMatchIndex = 0;
+RustCHOP::getGeneralInfo(CHOP_GeneralInfo *ginfo, const OP_Inputs *inputs, void *reserved1) {
+    ginfo->cookEveryFrameIfAsked = true;
+    ginfo->timeslice = true;
+    ginfo->inputMatchIndex = 0;
 }
 
 bool
-RustCHOP::getOutputInfo(CHOP_OutputInfo* info, const OP_Inputs* inputs, void* reserved1)
-{
+RustCHOP::getOutputInfo(CHOP_OutputInfo *info, const OP_Inputs *inputs, void *reserved1) {
     ChopOutputInfo ci;
     ChopOperatorInputs opIn;
     for (auto i = 0; i < inputs->getNumInputs(); i++) {
@@ -113,16 +93,14 @@ RustCHOP::getOutputInfo(CHOP_OutputInfo* info, const OP_Inputs* inputs, void* re
 }
 
 void
-RustCHOP::getChannelName(int32_t index, OP_String *name, const OP_Inputs* inputs, void* reserved1)
-{
-	name->setString("chan1");
+RustCHOP::getChannelName(int32_t index, OP_String *name, const OP_Inputs *inputs, void *reserved1) {
+    name->setString("chan1");
 }
 
 void
-RustCHOP::execute(CHOP_Output* output,
-							  const OP_Inputs* inputs,
-							  void* reserved)
-{
+RustCHOP::execute(CHOP_Output *output,
+                  const OP_Inputs *inputs,
+                  void *reserved) {
     ChopOperatorInputs ins;
     ins.inputs = rust::Vec<ChopOperatorInput>();
     for (int i = 0; i < inputs->getNumInputs(); i++) {
@@ -137,49 +115,44 @@ RustCHOP::execute(CHOP_Output* output,
     out.num_samples = output->numSamples;
     out.sample_rate = output->sampleRate;
     chop->execute(&out, &ins);
-    for (int i = 0; i < output->numChannels; i++) {
+    for (int i = 0; i < out.channels.size(); i++) {
         auto c = out.channels[i];
         output->channels[i] = c.data.data();
     }
 }
 
 int32_t
-RustCHOP::getNumInfoCHOPChans(void * reserved1)
-{
-	return 0;
+RustCHOP::getNumInfoCHOPChans(void *reserved1) {
+    return 0;
 }
 
 void
 RustCHOP::getInfoCHOPChan(int32_t index,
-										OP_InfoCHOPChan* chan,
-										void* reserved1)
-{
+                          OP_InfoCHOPChan *chan,
+                          void *reserved1) {
     // TODO:
 }
 
-bool		
-RustCHOP::getInfoDATSize(OP_InfoDATSize* infoSize, void* reserved1)
-{
+bool
+RustCHOP::getInfoDATSize(OP_InfoDATSize *infoSize, void *reserved1) {
     // TODO support dat
-	return false;
+    return false;
 }
 
 void
 RustCHOP::getInfoDATEntries(int32_t index,
-										int32_t nEntries,
-										OP_InfoDATEntries* entries, 
-										void* reserved1)
-{
+                            int32_t nEntries,
+                            OP_InfoDATEntries *entries,
+                            void *reserved1) {
 
     // TODO map dat
 }
 
 void
-RustCHOP::setupParameters(OP_ParameterManager* manager, void *reserved1)
-{
+RustCHOP::setupParameters(OP_ParameterManager *manager, void *reserved1) {
     // TODO support all param types
-    for (auto param : chop->get_params().params) {
-        OP_NumericParameter    np;
+    for (auto param: chop->get_params().params) {
+        OP_NumericParameter np;
 
         np.name = param.name.c_str();
         np.label = param.label.c_str();
@@ -196,17 +169,15 @@ RustCHOP::setupParameters(OP_ParameterManager* manager, void *reserved1)
     }
 }
 
-void 
-RustCHOP::pulsePressed(const char* name, void* reserved1)
-{
-	if (!strcmp(name, "Reset"))
-	{
+void
+RustCHOP::pulsePressed(const char *name, void *reserved1) {
+    if (!strcmp(name, "Reset")) {
         chop->on_reset();
     }
 }
 
 ChopOperatorInput
-RustCHOP::mapInput(const OP_CHOPInput* input) {
+RustCHOP::mapInput(const OP_CHOPInput *input) {
     ChopOperatorInput chop;
     chop.id = input->opId;
     chop.path = input->opPath;
@@ -218,7 +189,7 @@ RustCHOP::mapInput(const OP_CHOPInput* input) {
     int ind = 0;
     for (auto i = 0; i < input->numChannels; i++) {
         ChopChannel chan;
-        chan .data = rust::Vec<float>();
+        chan.data = rust::Vec<float>();
         for (auto j = 0; j < input->numSamples; j++) {
             chan.data.push_back(input->getChannelData(i)[ind]);
             ind++;
