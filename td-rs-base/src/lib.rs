533bdc890cdccf19f41040dab34333e9f5e72315
Charlotte McElwain <charlotte.c.mcelwain@gmail.com>
2023-05-05 20:07:52 -0500

    Update docs.

--- td-rs-base/src/lib.rs
+++ td-rs-base/src/lib.rs
@@ -2,16 +2,23 @@ pub mod cxx;
 pub mod operator_input;
 pub mod parameter_manager;
 
+use std::ops::{Deref, DerefMut};
+use std::path::PathBuf;
 pub use operator_input::OperatorInput;
 pub use parameter_manager::ParameterManager;
 
 use crate::cxx::ffi::{NumericParameter, StringParameter};
 
+/// Trait for defining operator parameters.
 pub trait OperatorParams {
+    /// Register parameters with the parameter manager.
     fn register(&mut self, parameter_manager: &mut ParameterManager);
+    /// Update parameters from operator input.
     fn update(&mut self, input: &OperatorInput);
 }
 
+/// Options for creating parameters in derive macro.
+/// Not intended for direct use.
 pub struct ParamOptions {
     pub name: String,
     pub label: String,
@@ -46,8 +53,11 @@ impl From<ParamOptions> for StringParameter {
     }
 }
 
+/// Trait for implementing parameter types.
 pub trait Param {
+    /// Register parameter with the parameter manager.
     fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager);
+    /// Update parameter from operator input.
     fn update(&mut self, name: &str, input: &OperatorInput);
 }
 
@@ -100,7 +110,9 @@ impl_param_float!(f64);
 
 impl Param for String {
     fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager) {
-        parameter_manager.append_string(options.into());
+        let mut param: StringParameter = options.into();
+        param.default_value = self.clone();
+        parameter_manager.append_string(param);
     }
 
     fn update(&mut self, name: &str, input: &OperatorInput) {
@@ -110,7 +122,14 @@ impl Param for String {
 
 impl Param for rgb::RGB8 {
     fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager) {
-        parameter_manager.append_rgb(options.into());
+        let mut param: NumericParameter = options.into();
+        param.default_values = [
+            self.r as f64,
+            self.g as f64,
+            self.b as f64,
+            0.0,
+        ];
+        parameter_manager.append_rgb(param);
     }
 
     fn update(&mut self, name: &str, input: &OperatorInput) {
@@ -124,7 +143,14 @@ impl Param for rgb::RGB8 {
 
 impl Param for rgb::RGB16 {
     fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager) {
-        parameter_manager.append_rgb(options.into());
+        let mut param: NumericParameter = options.into();
+        param.default_values = [
+            self.r as f64,
+            self.g as f64,
+            self.b as f64,
+            0.0,
+        ];
+        parameter_manager.append_rgb(param);
     }
 
     fn update(&mut self, name: &str, input: &OperatorInput) {
@@ -138,7 +164,14 @@ impl Param for rgb::RGB16 {
 
 impl Param for rgb::RGBA8 {
     fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager) {
-        parameter_manager.append_rgba(options.into());
+        let mut param: NumericParameter = options.into();
+        param.default_values = [
+            self.r as f64,
+            self.g as f64,
+            self.b as f64,
+            self.a as f64,
+        ];
+        parameter_manager.append_rgba(param);
     }
 
     fn update(&mut self, name: &str, input: &OperatorInput) {
@@ -153,7 +186,14 @@ impl Param for rgb::RGBA8 {
 
 impl Param for rgb::RGBA16 {
     fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager) {
-        parameter_manager.append_rgba(options.into());
+        let mut param: NumericParameter = options.into();
+        param.default_values = [
+            self.r as f64,
+            self.g as f64,
+            self.b as f64,
+            self.a as f64,
+        ];
+        parameter_manager.append_rgba(param);
     }
 
     fn update(&mut self, name: &str, input: &OperatorInput) {
@@ -163,5 +203,75 @@ impl Param for rgb::RGBA16 {
             input.get_int(name, 2) as u16,
             input.get_int(name, 3) as u16,
         );
+    }
+}
+
+/// A parameter wrapping a `PathBuf` that will be registered as a folder parameter.
+pub struct Folder(PathBuf);
+
+impl Deref for Folder {
+    type Target = PathBuf;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Folder {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Param for Folder {
+    fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager) {
+        let mut param: StringParameter = options.into();
+        param.default_value = self.to_string_lossy().to_string();
+        parameter_manager.append_folder(param);
+    }
+
+    fn update(&mut self, name: &str, input: &OperatorInput) {
+        self.0 = PathBuf::from(input.get_string(name));
+    }
+}
+
+/// A parameter wrapping a `PathBuf` that will be registered as a file parameter.
+pub struct File(PathBuf);
+
+impl Deref for File {
+    type Target = PathBuf;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for File {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Param for File {
+    fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager) {
+        let mut param: StringParameter = options.into();
+        param.default_value = self.to_string_lossy().to_string();
+        parameter_manager.append_file(param);
+    }
+
+    fn update(&mut self, name: &str, input: &OperatorInput) {
+        self.0 = PathBuf::from(input.get_string(name));
+    }
+}
+
+impl Param for PathBuf {
+    fn register(&self, options: ParamOptions, parameter_manager: &mut ParameterManager) {
+        let mut param: StringParameter = options.into();
+        param.default_value = self.to_string_lossy().to_string();
+        parameter_manager.append_file(param);
+    }
+
+    fn update(&mut self, name: &str, input: &OperatorInput) {
+        *self = PathBuf::from(input.get_string(name));
     }
 }
