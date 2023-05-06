f36c23f111d30e036b0976daf7b743b8d6dec309
Charlotte McElwain <charlotte.c.mcelwain@gmail.com>
2023-05-05 20:08:05 -0500

    Remove primitiveInfo.

--- td-rs-base/src/RustBase.h
+++ td-rs-base/src/RustBase.h
@@ -4,23 +4,6 @@
 #ifndef TD_RS_RUSTBASE_H
 #define TD_RS_RUSTBASE_H
 
-struct RS_PrimitiveInfo {
-    int32_t			numVertices;
-    const int32_t*	pointIndices;
-    PrimitiveType	type;
-    int32_t			pointIndicesOffset;
-};
-
-std::unique_ptr<RS_PrimitiveInfo> getPrimitiveInfo(const OP_SOPInput* input, int32_t index) {
-    auto info = input->getPrimitive(index);
-    return std::make_unique<RS_PrimitiveInfo>(RS_PrimitiveInfo {
-        .numVertices = info.numVertices,
-        .pointIndices = info.pointIndices,
-        .type = info.type,
-        .pointIndicesOffset = info.pointIndicesOffset,
-    });
-}
-
 void setString(OP_String *dest, const char *src) {
     dest->setString(src);
 }
